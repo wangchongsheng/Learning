7e8d834418ed7ade8d5f65e77c82ec3f4cabd0d8
wangchongsheng <wang_chongsheng@163.com>
2018-01-12 17:57:07 +0800

    123

--- jingdong_app/app01/views.py
+++ jingdong_app/app01/views.py
@@ -10,12 +10,13 @@ def csking(req):
     print('前端数据GET',req.GET)
     print('前端数据POST',req.POST)
     print ('web data',req.FILES)
-    for item in req.FILES:
-        obj=req.FILES.get(item)
-        filename = obj.name
-        f=open(filename,'w')
-        for line in obj.chuck():
-            f.write(line)
-        f.close()
+    # for item in req.FILES:
+    #     obj=req.FILES.get(item)
+    #     filename = obj.name
+    #
+    #     f=open(filename,'wb')
+    #     for line in obj.chunks():
+    #         f.write(line)
+    #     f.close()
 
     return  render(req,'index.html')
